2a8744ab0ec3757e8e27dc95f3749029e96db0ab
Anesu Maseko <masekoanesuc@gmail.com>
2024-08-14 13:18:25 +0200

    imported progress bar from mui and positioned  it correctly

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -7,18 +7,24 @@ import { faPaw } from "@fortawesome/free-solid-svg-icons";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
 
 function Explore() {
   const [breeds, setBreeds] = useState([]);
   const [images, setImages] = useState({});
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredBreeds, setFilteredBreeds] = useState([]);
+  const [loadingImages, setLoadingImages] = useState(true); // Add loadingImages state
 
   useEffect(() => {
     async function loadData() {
       const breedData = await fetchBreeds();
       setBreeds(breedData);
+      setFilteredBreeds(breedData);
 
+      // Fetch images
+      setLoadingImages(true); // Start loading images
       const imagePromises = breedData.map(async (breed) => {
         const imageData = await fetchImagesByBreed(breed.id);
         return { breed, image: imageData[0] };
@@ -31,7 +37,7 @@ function Explore() {
       }, {});
 
       setImages(imageMap);
-      setFilteredBreeds(breedData);
+      setLoadingImages(false); // End loading images
     }
 
     loadData();
@@ -64,11 +70,25 @@ function Explore() {
           />
         </Stack>
       </div>
+      {loadingImages && (
+        <Box sx={{ width: "100%", marginBottom: 2 }}>
+          <LinearProgress
+            sx={{
+              backgroundColor: "#89909F",
+              "& .MuiLinearProgress-bar": {
+                backgroundColor: "#e0e0e0",
+              },
+            }}
+          />
+        </Box>
+      )}
+
       <div className="dogList">
         {filteredBreeds.map((breed) => (
           <div key={breed.id} className="dogItem">
             <Link to="/MoreInfo">
               <h2>{breed.name}</h2>
+              <p>{breed.temperament}</p>
               {images[breed.id] && (
                 <img
                   src={images[breed.id].url}
@@ -76,14 +96,14 @@ function Explore() {
                   className="dogImage"
                 />
               )}
-              <p>{breed.temperament}</p>
             </Link>
           </div>
         ))}
       </div>
+
       <footer className="footer">
         <p>
-          <FontAwesomeIcon icon={faPaw} size="1x" style={{ color: "black" }} />{" "}
+          <FontAwesomeIcon icon={faPaw} size="1x" style={{ color: "black" }} />
           2024 Dogs 101. All rights reserved.
         </p>
       </footer>
